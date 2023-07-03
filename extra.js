d5ae8926647fdb047f10fddee412b9c740e49957
naso <naso@epicode.it>
2023-07-03 10:57:15 +0200

    Esercizio selettori

---
+++ extra.js
@@ -0,0 +1,25 @@
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+ // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+
+//1
+ const twitter = document.querySelector(".p-4:nth-of-type(3) .list-unstyled li:nth-of-type(2)  a");
+
+ twitter.remove()
+
+ window.addEventListener('load') => {
+
+ }
+
+//2
+
+
+
+
+//3
+
+let autori = document.querySelectorAll('.blog-post-meta');
+
+for(let element of autori){
+    
+}
